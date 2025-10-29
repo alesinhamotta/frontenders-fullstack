18c56986e830c428bd2a6f78d4d1d5617c47b0cf
alesinhamotta <smartminds.mktdigital@gmail.com>
2025-10-29 12:46:57 -0300

    Primeiros passos

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+    <h1>Hello, World com Flask!</h1>
+    <nav>
+        <a href="/sobre">Sobre</a> - 
+        <a href="/contatos">Contatos</a>
+    </nav>
+    '''
+
+@app.route('/sobre')
+def about_page():
+    return 'Página sobre'
+
+@app.route("/contatos")
+def contacts_page():
+    contato = '<h1>Faça contato</h1>'
+    return contato
+
+if __name__ == '__main__':
+    app.run(debug=True)
