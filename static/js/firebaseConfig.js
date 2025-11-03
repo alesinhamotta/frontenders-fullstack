6976bd673d35193631a8077d8191242a6ed7362b
alesinhamotta <smartminds.mktdigital@gmail.com>
2025-11-03 12:51:04 -0300

    templetes ok

--- static/js/firebaseConfig.js
+++ static/js/firebaseConfig.js
@@ -1,1 +1,8 @@
-/**Vazio por enquanto */
+const firebaseConfig = {
+  apiKey: "AIzaSyAe4M5rj2wzeuf4cjTDPYss-XAbJr0LdjE",
+  authDomain: "frontenders-ed43d.firebaseapp.com",
+  projectId: "frontenders-ed43d",
+  storageBucket: "frontenders-ed43d.firebasestorage.app",
+  messagingSenderId: "251648408471",
+  appId: "1:251648408471:web:45da34f88dcafe9c3462d2"
+};
