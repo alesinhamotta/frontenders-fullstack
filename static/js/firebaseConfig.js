abcb14be88a71f17f94224379ff89e889a2f205e
alesinhamotta <smartminds.mktdigital@gmail.com>
2025-11-12 09:32:31 -0300

    teste

--- static/js/firebaseConfig.js
+++ static/js/firebaseConfig.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAe4M5rj2wzeuf4cjTDPYss-XAbJr0LdjE",
-  authDomain: "frontenders-ed43d.firebaseapp.com",
-  projectId: "frontenders-ed43d",
-  storageBucket: "frontenders-ed43d.firebasestorage.app",
-  messagingSenderId: "251648408471",
-  appId: "1:251648408471:web:45da34f88dcafe9c3462d2"
+  apiKey: "AIzaSyBHC6fxBShUXjcdjBFjJ8k1bF34HKKVP3c",
+  authDomain: "frontenders-1d1c1.firebaseapp.com",
+  projectId: "frontenders-1d1c1",
+  storageBucket: "frontenders-1d1c1.firebasestorage.app",
+  messagingSenderId: "991944134559",
+  appId: "1:991944134559:web:df606bb7575a07489e77aa"
 };
