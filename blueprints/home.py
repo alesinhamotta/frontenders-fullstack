8a749e3743ecc6d145f4c51db5a32978a702f474
alesinhamotta <smartminds.mktdigital@gmail.com>
2025-11-19 10:35:51 -0300

    debug ok

--- blueprints/home.py
+++ blueprints/home.py
@@ -28,7 +28,7 @@ def home_page():
             own_id, own_display_name, own_photo_url,
             SUBSTR(pad_content, 1, 30) || '...' AS pad_content_preview
         FROM pads
-        INNER JOIN owners ON pad_owner = own_id 
+        INNER JOIN owners ON pad_owner = own_uid
             WHERE pad_status = 'ON'
             ORDER BY pad_created_at DESC;
     ''')
